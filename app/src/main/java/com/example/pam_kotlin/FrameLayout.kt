31852786043b08ede392a0158818ad2e7afd0017
enjoyghozi <adamghosi03@gmail.com>
2023-10-05 22:35:47 +0700

    activity layout

---
+++ app/src/main/java/com/example/pam_kotlin/FrameLayout.kt
@@ -0,0 +1,11 @@
+package com.example.pam_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrameLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
